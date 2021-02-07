4489000c7d51963886210ab9fad39da1d75564a5
LauraAbCoronel <68669949+LauraAbCoronel@users.noreply.github.com>
2021-02-07 17:52:04 -0500

    Job Role other input show/hide

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-// finds the input element for name and sets it to nameInput
+// nameInput contains the input element for name
 const nameInput = document.querySelector('#name');
 // sets the name field to focus so when page loads name input is selected.
 nameInput.focus();
+
+//otherJob contains the input element for other Job text input
+const otherJob = document.querySelector('#other-job-role');
+//as default we want to have it hidden
+otherJob.style.display = 'none';
+const jobRole = otherJob.previousElementSibling;
+jobRole.addEventListener('change',() =>{
+    if (jobRole.value === 'other') {
+        otherJob.style.display = '';
+    } else {
+        otherJob.style.display = 'none';
+    }
+})
