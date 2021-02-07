5cea51b0b3bd29261dddcbb13d2c19349f84d1f1
LauraAbCoronel <68669949+LauraAbCoronel@users.noreply.github.com>
2021-02-07 17:23:21 -0500

    Name input focused

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// finds the input element for name and sets it to nameInput
+const nameInput = document.querySelector('#name');
+// sets the name field to focus so when page loads name input is selected.
+nameInput.focus();
